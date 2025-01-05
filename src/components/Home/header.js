d68f534f7f984fb2354a3191b3ea48694c11f6bc
peterbroadhead <p.t.broadhead@gmail.com>
2025-01-05 22:18:35 +0000

    updates

--- src/components/Home/header.js
+++ src/components/Home/header.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import hero from "../../images/removed-Holographic.webp"
+import studioImage from "../../../static/img/studio.jpg"
 
 const Header = () => {
   return (
     <Fade duration={2200}>
       <div className="max-w-7xl mx-auto lg:px-6 md:px-3">
-        <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-0 xl:mt-28 flex gap-10 lg:flex-justify lg:flex flex-col lg:flex-row flex-col-reverse">
+        <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-0 xl:mt-28 flex gap-10 lg:flex-justify lg:flex flex-col lg:flex-row flex-col-reverse mb-32">
           <div className="text-center lg:text-left flex flex-col justify-center">
             <h1 className="text-black text-4xl font-semibold opacity-70">
               BESPOKE PROFESSIONAL
@@ -59,7 +60,7 @@ const Header = () => {
           <div className="lg:inset-y-0 lg:right-0 lg:w-1/2 my-4 flex lg:justify-end justify-center">
             <img
               className="rounded-xl w-auto h-full object-cover flex justify-center"
-              src={hero}
+              src={studioImage}
               alt="Voice recording studio"
             ></img>
           </div>
